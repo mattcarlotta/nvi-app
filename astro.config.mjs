5b608aba1577a948c35b70d2cd29c52dd33732d8
Matt <matt@mattcarlotta.com>
2023-09-30 13:47:51 -0700

    chore(astro.config.js): use vercel adapter instead of standalone node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
-import node from "@astrojs/node";
-
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
+    integrations: [solidJs(), tailwind()],
+    output: "server",
+    adapter: vercel(),
 });
