99e4935242be80d61c300249df3aadc4656d4408
Matt Carlotta <matt@mattcarlotta.com>
2023-11-16 11:04:52 -0800

    fix(layout): add padding top for non-auth pages

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -65,7 +65,7 @@ export default function LoginForm(props: LoginFormProps) {
 
 
     return (
-        <div class="flex flex-col justify-center items-center space-y-4">
+        <div class="flex flex-col justify-center items-center space-y-4 mt-10">
             <div class="flex flex-col space-y-4 w-full max-w-xl p-8 bg-gray-900 rounded">
                 <h1 class="text-center text-3xl">Login</h1>
                 <form ref={formRef} id="login-form" class="w-full" onSubmit={handleSubmit}>
